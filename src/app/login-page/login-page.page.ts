1595ad15a669b67f53ef3c5f9ecbc6e81cf946a2
Emily Daly <edaly3@luc.edu>
2020-12-22 11:56:52 -0600

    updated branch

--- src/app/login-page/login-page.page.ts
+++ src/app/login-page/login-page.page.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Md5 } from 'ts-md5/dist/md5'
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-login-page',
@@ -13,6 +14,7 @@ export class LoginPagePage implements OnInit {
 
   login_form: FormGroup;
   api_data: string;
+  http: any;
 
   constructor(
     public formBuilder: FormBuilder,
@@ -26,22 +28,25 @@ export class LoginPagePage implements OnInit {
       password: new FormControl('', Validators.required)
     });
   }
-/*
+
   onSubmit(values) {
     console.log(values);
-    this.router.navigate(["/tab1"]); //temporarily routes to home page
+    //this.router.navigate(["/tab1"]); //temporarily routes to home page
   }
   
+  /*
   sendGetRequest() {
-    this.http.get()
+    
   }
   */
  
+  //document.getElementById("login-submit").addEventListener("click", getUserDescription, false);
+
   getUserDescription(handle:string,password:string) {
     console.log(handle,password);
-    //isValid: false;
-    console.log(this.httpClient.get('https://sparkfly.us/api/v0/usr/'+handle+'/'+Md5.hashStr(password)+'/description'));
-
+    const description = this.httpClient.get('https://sparkfly.us/api/v0/usr/'+handle+'/'+Md5.hashStr(password)+'/description');
+    JSON.stringify(description);
+    console.log(description);
   } 
 
 }
