b836a3d4911e8d60abb4167ea16f62feb9b3dd3c
Emily Daly <edaly3@luc.edu>
2021-02-07 17:04:08 -0600

    added suggestions page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,6 +43,10 @@ const routes: Routes = [
   },  {
     path: 'change-password',
     loadChildren: () => import('./change-password/change-password.module').then( m => m.ChangePasswordPageModule)
+  },
+  {
+    path: 'suggestions',
+    loadChildren: () => import('./suggestions/suggestions.module').then( m => m.SuggestionsPageModule)
   }
 
 
