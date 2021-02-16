752f50650b702a548e272b544042081f31ecc13e
Emily Daly <edaly3@luc.edu>
2021-02-16 12:08:23 -0600

    updated router links for all pages, added a service provider for calendar (not implemented)

--- src/app/login-page/login-page.page.ts
+++ src/app/login-page/login-page.page.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-//import { Observable } from 'rxjs';
 import { RestService } from '../services/rest-service/rest.service';
-//import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -19,7 +17,6 @@ export class LoginPagePage implements OnInit {
   constructor(
     public formBuilder: FormBuilder,
     private router: Router,
-    //private activatedRoute: ActivatedRoute,
     public httpClient: HttpClient,
     public restService: RestService
   ) { }
---
+++ src/app/services/calendar-service/calendar.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Calendar } from '@ionic-native/calendar/ngx';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CalendarServiceService {
+
+  constructor(private calendar: Calendar) { }
+
+  /*
+  this.calendar.createCalendar('MyCalendar').then(
+    (msg) => { console.log(msg); },
+    (err) => { console.log(err); }
+  );
+  */
+}
